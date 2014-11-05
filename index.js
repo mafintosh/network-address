b2df7ed22db0a7150df7616f422665fa151cdf7d
Mathias Buus <mathiasbuus@gmail.com>
2014-11-05 05:42:58 +0100

    pick last ip instead of first

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var os = require('os');
 module.exports = function() {
 	var interfaces = os.networkInterfaces();
 	for (var i in interfaces) {
-		for (var j = 0; j < interfaces[i].length; j++) {
+		for (var j = interfaces[i].length-1; j >= 0; j--) {
 			var face = interfaces[i][j];
 			if (!face.internal && face.family === 'IPv4') return face.address;
 		}
